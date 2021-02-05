12daca807879f57933e73e517b86e2d86fd5e64c
TFive <t5_@outlook.co.th>
2021-02-05 17:04:27 +0700

    Updated 2.1
    - Moved getFeatureList to Main.cpp.
    - Fixed force load menu button
    - Updated README.md and README-MOBILE.md. Be sure to read it again
    - Added link to video tutorial on README-MOBILE.md. Thanks Mahmoud Gaming
    
    And we forgot to thank RAUNAK MODS for a big help testing the template during v2.0

--- app/src/main/jni/Menu.h
+++ app/src/main/jni/Menu.h
@@ -42,38 +42,6 @@ Java_com_tfive_modmenu_FloatingModMenuService_IconWebViewData(JNIEnv *env, jobje
     iconValid = true;
     return NULL;
 }
-JNIEXPORT jobjectArray
-JNICALL
-Java_com_tfive_modmenu_FloatingModMenuService_settingsList(JNIEnv *env, jobject activityObject) {
-    jobjectArray ret;
-
-    const char *features[] = {
-            OBFUSCATE("0_Category_Settings"),
-            OBFUSCATE("-1_Toggle_Color animation"),
-            OBFUSCATE("-2_Toggle_Auto size vertically"),
-            OBFUSCATE("-3_Toggle_Save feature preferences (Radio Button is not saved)"), //-3 is checked on Preferences.java
-            OBFUSCATE("0_Category_Logcat"),
-            OBFUSCATE("0_RichTextView_Save logcat if a bug occured and sent it to the modder. Clear logcat and reproduce bug again if the log file is too large"),
-            OBFUSCATE("0_RichTextView_<small>Saving logcat does not need file permission. Logcat location:<br/>Android 11: /storage/emulated/0/Documents/Mod Menu logs/"
-                      "<br/>Android 10 and below: /storage/emulated/0/Android/data/(package name)/files/Mod Menu logs</small>"),
-            OBFUSCATE("-4_Button_Save logcat to file"),
-            OBFUSCATE("-5_Button_Clear logcat"),
-            OBFUSCATE("0_Category_Menu"),
-            OBFUSCATE("-6_Button_<font color='red'>Close settings</font>"),
-            //(TFiveEndCredit)
-    };
-
-    int Total_Feature = (sizeof features /
-                         sizeof features[0]); //Now you dont have to manually update the number everytime;
-    ret = (jobjectArray)
-            env->NewObjectArray(Total_Feature, env->FindClass(OBFUSCATE("java/lang/String")),
-                                env->NewStringUTF(""));
-    int i;
-    for (i = 0; i < Total_Feature; i++)
-        env->SetObjectArrayElement(ret, i, env->NewStringUTF(features[i]));
-
-    return (ret);
-}
 
 JNIEXPORT jobjectArray
 JNICALL
