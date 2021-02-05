d04c9104242b8fe7cb2e2dcb17af9797ee175bbc
TFive <t5_@outlook.co.th>
2021-02-05 16:57:33 +0700

    Updated 2.1
    - Moved getFeatureList to Main.cpp.
    - Fixed force load menu button
    - Updated README.md and README-MOBILE.md. Be sure to read it again
    - Added link to video tutorial on README-MOBILE.md. Thanks Mahmoud Gaming
    
    And we forgot to thank RAUNAK MODS for a big help testing the template during v2.0

--- app/src/main/jni/Menu.h
+++ app/src/main/jni/Menu.h
@@ -119,48 +119,4 @@ Java_com_tfive_modmenu_FloatingModMenuService_settingsList(JNIEnv *env, jobject 
 
     return (ret);
 }
-
-JNIEXPORT jobjectArray
-JNICALL
-Java_com_tfive_modmenu_FloatingModMenuService_getFeatureList(JNIEnv *env, jobject activityObject) {
-    jobjectArray ret;
-
-    const char *features[] = {
-            OBFUSCATE("0_Category_The Category"),
-            OBFUSCATE("1_Toggle_The toggle"),
-            OBFUSCATE("2_SeekBar_The slider_1_100"),
-            OBFUSCATE("3_SeekBar_Kittymemory slider example_1_5"),
-            OBFUSCATE("4_Spinner_The spinner_Items 1,Items 2,Items 3"),
-            OBFUSCATE("5_Button_The button"),
-            OBFUSCATE("0_ButtonLink_The button with link_https://www.youtube.com/"),
-            OBFUSCATE("6_ButtonOnOff_The On/Off button"),
-            OBFUSCATE("7_CheckBox_The Check Box"),
-            OBFUSCATE("200_InputValue_The input number"),
-            OBFUSCATE("100_RadioButton_Radio buttons_OFF,Mod 1,Mod 2,Mod 3"),
-            OBFUSCATE(
-                    "0_RichTextView_This is text view, not fully HTML."
-                    "<b>Bold</b> <i>italic</i> <u>underline</u>"
-                    "<br />New line <font color='red'>Support colors</font>"),
-            OBFUSCATE(
-                    "0_RichWebView_<html><head><style>body{color: white;}</style></head><body>"
-                    "This is WebView, with REAL HTML support!"
-                    "<div style=\"background-color: darkblue; text-align: center;\">Support CSS</div>"
-                    "<marquee style=\"color: green; font-weight:bold;\" direction=\"left\" scrollamount=\"5\" behavior=\"scroll\">This is <u>scrollable</u> text</marquee>"
-                    "</body></html>")
-    };
-
-    int Total_Feature = (sizeof features /
-                         sizeof features[0]); //Now you dont have to manually update the number everytime;
-    ret = (jobjectArray)
-            env->NewObjectArray(Total_Feature, env->FindClass(OBFUSCATE("java/lang/String")),
-                                env->NewStringUTF(""));
-    int i;
-    for (i = 0; i < Total_Feature; i++)
-        env->SetObjectArrayElement(ret, i, env->NewStringUTF(features[i]));
-
-    pthread_t ptid;
-    pthread_create(&ptid, NULL, antiLeech, NULL);
-
-    return (ret);
-}
 }
